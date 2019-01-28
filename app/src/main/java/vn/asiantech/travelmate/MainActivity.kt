f26ccb6dfc71f6f4b845daf63e88d60137b112d7
Tai Nguyen D <tai.nguyen@asiantech.vn>
2019-01-28 10:50:02 +0700

    init project

---
+++ app/src/main/java/vn/asiantech/travelmate/MainActivity.kt
@@ -0,0 +1,12 @@
+package vn.asiantech.travelmate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
