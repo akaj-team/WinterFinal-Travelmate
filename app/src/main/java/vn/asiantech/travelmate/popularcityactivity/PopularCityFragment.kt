f0863072113128e22ab99d7d7b619bca29ab2237
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2019-02-13 14:42:18 +0700

    load data

--- app/src/main/java/vn/asiantech/travelmate/popularcityactivity/PopularCityFragment.kt
+++ app/src/main/java/vn/asiantech/travelmate/popularcityactivity/PopularCityFragment.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,45 +14,52 @@ import kotlinx.android.synthetic.main.fragment_popular_city.*
 import vn.asiantech.travelmate.R
 import vn.asiantech.travelmate.models.City
 
-class PopularCityFragment : Fragment() {
-    private lateinit var database : DatabaseReference
-    private var firebase : FirebaseDatabase? = FirebaseDatabase.getInstance()
+class PopularCityFragment : Fragment(), PopularCityAdapter.OnItemClickListener {
+    private lateinit var database: DatabaseReference
+    private var firebase: FirebaseDatabase? = FirebaseDatabase.getInstance()
     private lateinit var listCity: ArrayList<City>
     private lateinit var popularCityAdapter: PopularCityAdapter
     private var recyclerView: RecyclerView? = null
     private lateinit var viewManager: GridLayoutManager
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_popular_city, container, false)
-        initRecyclerView()
+        initData()
         return view
     }
 
-    private fun initRecyclerView() {
+    private fun initData() {
         listCity = ArrayList()
         database = firebase!!.getReference("travel")
-        database.addValueEventListener(object : ValueEventListener, PopularCityAdapter.OnItemClickListener {
-            override fun onClicked(position: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                //nothing
             }
 
-            override fun onCancelled(p0: DatabaseError) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
             override fun onDataChange(p0: DataSnapshot) {
                 for (image in p0.children) {
                     val city = image.getValue(City::class.java)!!
                     listCity.add(city)
                 }
-                viewManager = GridLayoutManager(context, 2)
-                popularCityAdapter = PopularCityAdapter(listCity, this)
-                recyclerView = recyclerViewPopularCity?.apply {
-                    setHasFixedSize(true)
-                    layoutManager = viewManager
-                    adapter = popularCityAdapter
-                    addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL))
-                    addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-                }
+                initRecyclerView()
             }
         })
     }
+
+    private fun initRecyclerView() {
+        viewManager = GridLayoutManager(context, 2)
+        popularCityAdapter = PopularCityAdapter(listCity, this)
+        recyclerView = recyclerViewPopularCity?.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = popularCityAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL))
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+    }
+
+    override fun onClicked(position: Int) {
+        Log.i("xxxxxx", listCity.get(position).name.toString())
+    }
+
 }
+
