c9681da272a022bcd6d5c084eb1b613da98dc29d
at-dinhtruong <dinh.truong@asiantech.vn>
2019-02-14 10:45:20 +0700

    add action menu drawer

--- app/src/main/java/vn/asiantech/travelmate/popularcityactivity/PopularCityActivity.kt
+++ app/src/main/java/vn/asiantech/travelmate/popularcityactivity/PopularCityActivity.kt
@@ -1,8 +1,9 @@
 package vn.asiantech.travelmate.popularcityactivity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.v4.content.ContextCompat
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -12,6 +13,7 @@ import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_popular_city.*
 import vn.asiantech.travelmate.R
+import vn.asiantech.travelmate.login.LoginActivity
 
 class PopularCityActivity : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
@@ -54,15 +56,23 @@ class PopularCityActivity : AppCompatActivity(), View.OnClickListener, Navigatio
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragment = supportFragmentManager.findFragmentById(R.id.frameLayoutDrawer)
         when (item.itemId) {
             R.id.navDestination -> {
-                // Handle the camera action
+                if (fragment is PopularCityFragment) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    val intent = Intent(this, PopularCityActivity::class.java)
+                    startActivity(intent)
+                }
+
             }
             R.id.navHotel -> {
 
             }
             R.id.navLogout -> {
-
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
             }
             R.id.navSetting -> {
 
@@ -75,8 +85,7 @@ class PopularCityActivity : AppCompatActivity(), View.OnClickListener, Navigatio
 
     private fun initView() {
         val actionBar = supportActionBar
-        actionBar?.title = "TravelMate"
-        val drawer= ContextCompat.getDrawable(applicationContext,R.drawable.ic_search_white_24dp)
+        actionBar?.title = getString(R.string.populatFragmentTravelMate)
     }
 
     override fun onClick(v: View?) {
