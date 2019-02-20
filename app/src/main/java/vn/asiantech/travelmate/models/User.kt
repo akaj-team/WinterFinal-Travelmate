bba9677f6a399fb81fb26167733f7e194392e2b9
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2019-02-20 16:11:08 +0700

    view popular travel and information userlogin

--- app/src/main/java/vn/asiantech/travelmate/models/User.kt
+++ app/src/main/java/vn/asiantech/travelmate/models/User.kt
@@ -1,9 +1,12 @@
 package vn.asiantech.travelmate.models
 
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 data class User(
-    val avatar: String,
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val password: String
+    var avatar: String? = "",
+    var firstName: String? = "",
+    var lastName: String? = "",
+    var email: String? = "",
+    var password: String = ""
 )
--- app/src/main/java/vn/asiantech/travelmate/popularcityactivity/PopularCityActivity.kt
+++ app/src/main/java/vn/asiantech/travelmate/popularcityactivity/PopularCityActivity.kt
@@ -6,31 +6,59 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import vn.asiantech.travelmate.R
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_popular_city.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+import vn.asiantech.travelmate.R
+import vn.asiantech.travelmate.models.User
+import vn.asiantech.travelmate.utils.Constant
+import vn.asiantech.travelmate.utils.ValidationUtil
 
 class PopularCityActivity : AppCompatActivity(), View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
-
+    private var database: DatabaseReference? = null
+    private var firebaseAuth: FirebaseAuth? = FirebaseAuth.getInstance()
+    private var fireBaseUser: FirebaseUser? = firebaseAuth?.currentUser
     var progressDialog: ProgressDialog? = null
+    var user: User? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_popular_city)
         progressDialog = ProgressDialog(this)
+        getInforUser()
         initDrawer()
         initView()
         initFragment()
     }
 
+    private fun getInforUser() {
+        val path = fireBaseUser?.email
+        database = FirebaseDatabase.getInstance().getReference(Constant.KEY_ACCOUNT)
+        database?.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                user = path?.let { ValidationUtil.getValuePathChild(it) }?.let { p0.child(it).getValue(User::class.java) }
+                val navigationView = findViewById<NavigationView>(R.id.navView)
+                val itemViewHeader = navigationView.getHeaderView(0)
+                val imgAvatar = itemViewHeader.imgAvatar
+                Glide.with(applicationContext).load(user?.avatar).into(imgAvatar)
+                itemViewHeader.tvName.text = user?.lastName
+            }
+        })
+    }
+
     private fun initDrawer() {
         setSupportActionBar(toolbar)
-        val toggle = ActionBarDrawerToggle(
-            this, drawerLayout, toolbar, R.string.navigationDrawerOpen, R.string.navigationDrawerClose
-        )
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigationDrawerOpen, R.string.navigationDrawerClose)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
@@ -88,6 +116,7 @@ class PopularCityActivity : AppCompatActivity(), View.OnClickListener, Navigatio
             .replace(R.id.frameLayoutDrawer, PopularCityFragment())
             .commit()
     }
+
     fun showProgressbarDialog() {
         progressDialog?.apply {
             setProgressStyle(ProgressDialog.STYLE_SPINNER)
