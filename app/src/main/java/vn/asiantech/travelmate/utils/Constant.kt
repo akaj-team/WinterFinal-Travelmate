8384033764acd178809eb980c062a7dfbd0e1edd
Phu Tran D.[4] <phu.tran4@asiantech.vn>
2019-03-05 11:20:11 +0700

    format code

--- app/src/main/java/vn/asiantech/travelmate/utils/Constant.kt
+++ app/src/main/java/vn/asiantech/travelmate/utils/Constant.kt
@@ -1,7 +1,6 @@
 package vn.asiantech.travelmate.utils
 
 object Constant {
-    const val PICK_IMAGE_REQUEST = 71
     const val CHECK_SIGNUP = "OK"
     const val KEY_ACCOUNT = "account"
     const val KEY_PASSWORD = "password"
